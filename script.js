1af08b9c5dbd745bc7fb2194dc02bfe4d3bf7094
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-12-05 15:46:14 -0800

    create increase/decrease salary functions

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // ------------------------- DOM elements -------------------------
 const main = document.getElementById('main');
-const doubleButton = document.getElementById('double');
+const increaseButton = document.getElementById('increase');
+const decreaseButton = document.getElementById('decrease');
 const showMillionairesButton = document.getElementById('show-millionaires');
-const addUserButton = document.getElementById('add-user');
+const addEmployeeButton = document.getElementById('add-user');
 const sortButton = document.getElementById('sort');
 const calculateWealthButton = document.getElementById('calculate-wealth');
 
 let data = [];
 
 getRandomUser();
-getRandomUser();
-getRandomUser();
+
 
 // ------------------------- functions -------------------------
 
@@ -32,23 +32,44 @@ async function getRandomUser() {
     salary: salary,
   };
 
-  console.log(newUser)
+  // console.log(newUser)
 
   // pass the new user into New Data
   addData(newUser);
+};
 
-}
+// function to increase the salary of th employee
+function increaseSalary(){
+
+  data = data.map((employee) => {
+    return {...employee, salary: employee.salary * 1.10}
+  });
+
+  updateDOM();
+};
+
+// function to decrease the salary of th employee
+function decreaseSalary(){
+
+  data = data.map((employee) => {
+
+    return {...employee, salary: employee.salary * .90}
+  });
+
+  updateDOM();
+};
+
 
 // push the new employee into our array with all the data
-function addData(employee){
+function addData(employee) {
   data.push(employee);
   console.log(data);
 
   updateDOM();
-}
+};
 
 // updating the DOM
-function updateDOM(employees = data){
+function updateDOM(employees = data) {
   // clear the main div
   main.innerHTML = `<h2><strong>Employee</strong> Salary</h2>`;
 
@@ -57,9 +78,18 @@ function updateDOM(employees = data){
     let element = document.createElement('div');
     // add the person class && style
     element.classList.add('person');
-    element.innerHTML = `<strong>${employee.name}</strong> ${employee.salary}`;
+    element.innerHTML = `<strong>${employee.name}</strong> ${formatSalary(employee.salary)}`;
     main.appendChild(element);
-
   });
-
 }
+
+// function to format the salary with ','  ~ stackoverflow
+function formatSalary(sal) {
+  return '$' + sal.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+
+// ------------------------- Event Listeners -------------------------
+addEmployeeButton.addEventListener('click', getRandomUser);
+increaseButton.addEventListener('click', increaseSalary);
+decreaseButton.addEventListener('click', decreaseSalary);
